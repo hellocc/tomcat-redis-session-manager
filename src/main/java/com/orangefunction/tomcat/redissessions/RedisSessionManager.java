b56613cb1096ef1dc77787299c83d6e89ed83bda
hellocc <815551282@qq.com>
2016-06-22 20:57:55 +0800

    Update RedisSessionManager.java
    dfsgsfsgf

--- src/main/java/com/orangefunction/tomcat/redissessions/RedisSessionManager.java
+++ src/main/java/com/orangefunction/tomcat/redissessions/RedisSessionManager.java
@@ -27,8 +27,7 @@ import redis.clients.jedis.Protocol;
 import redis.clients.util.Pool;
 
 public class RedisSessionManager extends ManagerBase implements Lifecycle {
-
-  enum SessionPersistPolicy {
+   enum SessionPersistPolicy {
     DEFAULT,
     SAVE_ON_CHANGE,
     ALWAYS_SAVE_AFTER_REQUEST;
